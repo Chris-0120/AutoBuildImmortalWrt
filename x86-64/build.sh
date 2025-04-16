816e9d3f02f70c2e8d539e12436042e93de72c9c
Chris-0120 <146090446+Chris-0120@users.noreply.github.com>
2025-04-16 10:37:35 +0800

    Update build.sh
    add frpc / xlnetacc

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -40,6 +40,9 @@ PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+# chris add 
+PACKAGES="$PACKAGES luci-app-frpc"
+PACKAGES="$PACKAGES luci-app-xlnetacc"
 
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
